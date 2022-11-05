13b8a0154a8330fc8a3ab606d65e209009f8393a
Kishor Bhagat <89726870+KishorBhagat@users.noreply.github.com>
2022-11-05 21:02:13 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -72,7 +72,7 @@ app.post('/login', async (req, res) => {
 
     } catch (error) {
         console.log(error);
-        res.status(500).send("Server error");
+        res.status(500).send(error);
     }
 });
 
